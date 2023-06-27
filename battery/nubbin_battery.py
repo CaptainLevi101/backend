d997837c635292bc5c8a94db064ab1aa3763822f
Puneet Kumar Singh <puneetkumarsingh10102003@gmail.com>
2023-06-27 14:00:10 +0530

    updated

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -11,5 +11,4 @@ class NubbinBattery(Battery):
         date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
-        else:
-            return False
+        return False
